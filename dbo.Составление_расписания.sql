f0bd4ed0bbe3ce07107f00115096cd2f46e4e073
SofiFairyTell <58175476+SofiFairyTell@users.noreply.github.com>
2021-09-23 22:05:13 +0300

    db

---
+++ dbo.Составление_расписания.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Составление_расписания] (
+    [Id]             INT           IDENTITY (1, 1) NOT NULL,
+    [Класс]          NCHAR (10)    NOT NULL,
+    [День_недели]    NVARCHAR (20) NOT NULL,
+    [Время_урока]    NCHAR (20)    NOT NULL,
+    [Предмет]        NCHAR (30)    NOT NULL,
+    [ФИО_учителя]    NCHAR (30)    NOT NULL,
+    [Номер_кабинета] NCHAR (3)     NOT NULL, 
+    CONSTRAINT [FK_Составление_расписания_Предмет] FOREIGN KEY ([Предмет]) REFERENCES [dbo].[Предмет], 
+    CONSTRAINT [PK_Составление_расписания] PRIMARY KEY ([Id]));
+
